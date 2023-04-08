5fce23e947569d31a02e595a10f843b6d6871623
Barry Stingmore <b.stingmore@hotmail.co.uk>
2023-04-08 08:51:52 +0100

    Add test objects

---
+++ script.py
@@ -0,0 +1,5 @@
+# 1. Create a list with the following destinations and save it into a variable called destinations.
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+
+# 2. Let's define a test traveler to see how our functionality is working so far. Create a test_traveler variable.
+test_traveler = ["Erin Wilkes", "Shanghai, China", ["Historical site", "art"]]
